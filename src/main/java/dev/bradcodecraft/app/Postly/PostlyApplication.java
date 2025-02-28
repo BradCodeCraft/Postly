43be7020de9caf9e14e5528058b17189237baeb4
bradcodecraft <bradcodecraft@gmail.com>
2025-02-28 18:44:07 -0500

    project initialized

---
+++ src/main/java/dev/bradcodecraft/app/Postly/PostlyApplication.java
@@ -0,0 +1,13 @@
+package dev.bradcodecraft.app.Postly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostlyApplication.class, args);
+	}
+
+}
